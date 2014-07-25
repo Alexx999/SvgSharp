fed3ca7f3f100092517c11d94f8daeeccf8c8809
Alexander Vostres <alex.vostres@gmail.com>
2014-07-25 14:30:37 +0300

    implement text element attributes

--- SvgSharp/Text.cs
+++ SvgSharp/Text.cs
@@ -4,13 +4,58 @@ namespace SvgSharp
 {
     public class Text : GraphicsElement
     {
+        public string Class { get; set; }
+        public string Style { get; set; }
+        public string ExternalResourcesRequired { get; set; }
+        public string Transform { get; set; }
+        public string LengthAdjust { get; set; }
+        public string X { get; set; }
+        public string Y { get; set; }
         public string Dx { get; set; }
         public string Dy { get; set; }
+        public string Rotate { get; set; }
+        public string TextLength { get; set; }
+        public string Value { get; set; }
 
         protected override void ReadAttribute(XmlReader reader)
         {
             switch (reader.Name)
             {
+                case "class":
+                {
+                    Class = reader.Value;
+                    return;
+                }
+                case "style":
+                {
+                    Style = reader.Value;
+                    return;
+                }
+                case "externalResourcesRequired":
+                {
+                    ExternalResourcesRequired = reader.Value;
+                    return;
+                }
+                case "transform":
+                {
+                    Transform = reader.Value;
+                    return;
+                }
+                case "lengthAdjust":
+                {
+                    LengthAdjust = reader.Value;
+                    return;
+                }
+                case "x":
+                {
+                    X = reader.Value;
+                    return;
+                }
+                case "y":
+                {
+                    Y = reader.Value;
+                    return;
+                }
                 case "dx":
                 {
                     Dx = reader.Value;
@@ -21,12 +66,27 @@ namespace SvgSharp
                     Dy = reader.Value;
                     return;
                 }
+                case "rotate":
+                {
+                    Rotate = reader.Value;
+                    return;
+                }
+                case "textLength":
+                {
+                    TextLength = reader.Value;
+                    return;
+                }
                 default:
                 {
                     base.ReadAttribute(reader);
                     return;
                 }
             }
+        }
+
+        protected override void ReadContent(XmlReader reader)
+        {
+            Value = reader.ReadElementContentAsString();
         }
     }
 }
